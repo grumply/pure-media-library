cf62f60703b93db22dbb46482329912bd9dc187d
Sean Hickman <seanmhickman@gmail.com>
2021-09-25 17:17:47 -0500

    Update to pure-sorcerer.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, stdenv, ghc, base, pure-elm, pure-txt, pure-intersection, pure-json, pure-lifted, pure-readfile, pure-stream, pure-sync, pure-time, pure-websocket, sorcerer, directory, filepath, hashable }:
+{ mkDerivation, stdenv, ghc, base, pure-elm, pure-txt, pure-intersection, pure-json, pure-lifted, pure-readfile, pure-stream, pure-sync, pure-time, pure-websocket, pure-sorcerer, directory, filepath, hashable }:
 mkDerivation {
   pname = "pure-media-library";
   version = "0.8.0.0";
@@ -15,7 +15,7 @@ mkDerivation {
     pure-sync
     pure-time
     pure-websocket
-    sorcerer
+    pure-sorcerer
     directory
     filepath
     hashable
